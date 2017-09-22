5372fa19a5004ab229152696c76973e84450fbba
liujiajia <jiajia_dead@qq.com>
2017-09-22 13:15:58 +0800

    提交第一个git工程

---
+++ git_first/src/cn/itcast/git/GitFirst.java
@@ -0,0 +1,13 @@
+package cn.itcast.git;
+/** 
+* @author 刘
+* @date   2017年9月22日 下午1:02:26 
+* 类说明 
+*/
+public class GitFirst {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello git");
+	}
+}
